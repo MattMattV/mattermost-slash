254b21868c239a9d81fff052f74619c9ab6aec14
Matthieu Vion <mvion@trace-software.com>
2016-09-09 16:49:35 +0200

    add possibility to insult people

--- index.php
+++ index.php
@@ -31,10 +31,37 @@ $slim->post('/', function(Request $request, Response $response) use ($slim) {
 
             $tmp = array(
                 'response_type' => 'in_channel',
-                'text' => "$sender tried to YOLO"
+                'text' => "@$sender tried to YOLO"
             );
 
             return $response->withJson($tmp, 200);
+    }
+
+    $tmp = array(
+        'response_type' => 'in_channel',
+        'text' => "something went wrong"
+    );
+
+    return $response->withJson($tmp, 200);
+
+});
+
+$slim->post('/fuck', function(Request $request, Response $response) use ($slim) {
+    $requestParams = $request->getParsedBody();
+    error_log($requestParams);
+
+    if(is_array($requestParams)) {
+        if(key_exists('user_name', $requestParams))
+            $sender = $requestParams['user_name'];
+        if(key_exists('command', $requestParams))
+            $command = $requestParams['command'];
+
+        $tmp = array(
+            'response_type' => 'in_channel',
+            'text' => "@$sender tried to insult someone by sending $command to me\n"
+        );
+
+        return $response->withJson($tmp, 200);
     }
 
     $tmp = array(
