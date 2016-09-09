52b6759a419678bc117e29ab100dc1f1e07c803d
Matthieu Vion <mvion@trace-software.com>
2016-09-09 16:57:51 +0200

    with mg body

--- index.php
+++ index.php
@@ -26,14 +26,13 @@ $slim->post('/', function(Request $request, Response $response) use ($slim) {
     error_log($requestParams);
 
     if(is_array($requestParams)) {
-        if(key_exists('user_name', $requestParams))
             $sender = $requestParams['user_name'];
-        if(key_exists('command', $requestParams))
             $command = $requestParams['command'];
+            $msg = $requestParams['text'];
 
         $tmp = array(
             'response_type' => 'in_channel',
-            'text' => "@$sender tried to insult someone by sending $command to me\n"
+            'text' => "@$sender tried to insult someone by sending $command $msg to me\n"
         );
 
         return $response->withJson($tmp, 200);
