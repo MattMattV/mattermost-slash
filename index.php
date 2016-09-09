2bc8a886b7f5c1d3fd9de97aaedea5c1230a0523
Matthieu Vion <mvion@trace-software.com>
2016-09-09 16:23:14 +0200

    response first try

--- index.php
+++ index.php
@@ -7,13 +7,33 @@ use Psr\Http\Message\ResponseInterface as Response;
 $config = [
     'settings' => [
         'displayErrorDetails' => true,
-        'determineRouteBeforeAppMiddleware' => true,
-    ]
+    ],
+
+    'logger' => function() {
+        $log = new Monolog\Logger('SLASHER');
+        $log->pushHandler(new Monolog\Handler\StreamHandler('php://stderr', Monolog\Logger::WARNING));
+
+        return $log;
+    }
 ];
 
 $slim = new App($config);
 
-$slim->get('/', function(Request $request, Response $response) {
+$slim->get('/', function(Request $request, Response $response) use ($slim) {
+    $requestParams = $request->getParsedBody();
+
+    if(is_array($requestParams)) {
+        if(key_exists('user_name', $requestParams))
+            $sender = $requestParams['user_name'];
+
+            $tmp = array(
+                'response_type' => 'in_channel',
+                'text' => "$sender tried to YOLO"
+            );
+
+            return $response->withJson($tmp, 200);
+    }
+
     print_r($request->getParsedBody());
 });
 
