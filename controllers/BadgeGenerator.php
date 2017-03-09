ed9535c0b7495168c581114b6f712c7526ad2d04
Matthieu Vion <mvion@trace-software.com>
2017-03-09 13:58:04 +0100

    BadgeGenerator have initial feature

--- controllers/BadgeGenerator.php
+++ controllers/BadgeGenerator.php
@@ -26,15 +26,38 @@ class BadgeGenerator
     public function __invoke(Request $request, Response $response)
     {
         $requestToken = $request->getParam('token');
+        $requester = $request->getParam('user_name');
+
+        $parameters = explode(' ', urldecode($request->getParam('text')));
+
+        $baseUrl = 'https://img.shields.io/badge/';
 
         $message = [
             'response_type' => 'in_channel',
-            'text' => ''
+            'username' => $requester,
+            'text' => 'wrong token'
         ];
-        
+
         if($this->container->get('badgeGeneratorToken') === $requestToken) {
-            $message['text'] = 'token ok';
+            if (count($parameters) >= 3) {
+                $message['text'] = "![Badge from $requester]($baseUrl"
+                    .urlencode($parameters[0])
+                    ."-"
+                    .urlencode($parameters[1])
+                    ."-"
+                    .urldecode($parameters[2]);
+
+                if (count($parameters) === 4) {
+                    $message['text'] .= "?style=".urlencode($parameters[3]);
+                }
+
+                $message['text'] .= ')';
+            } else {
+                $message['response_type'] = 'ephemeral';
+                $message['text'] = 'Problem with parameters';
+            }
         }
+
         return $response->withJson($message);
     }
 }
