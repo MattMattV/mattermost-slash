69fcd3c52fe4fc08d099fec3a7dcb8bd701934e8
Matthieu Vion <mvion@trace-software.com>
2017-06-29 12:27:45 +0200

    trying another image URL

--- controllers/Ah.php
+++ controllers/Ah.php
@@ -24,7 +24,7 @@ class Ah
 
     public function __invoke(Request $request, Response $response)
     {
-        $imageUrl = "https://pbs.twimg.com/profile_images/805774049892855808/Qw1m1Uvo.jpg";
+        $imageUrl = "https://images-cdn.9gag.com/photo/az81PXq_700b.jpg";
 
         $message = [
             'response_type' => 'in_channel',
